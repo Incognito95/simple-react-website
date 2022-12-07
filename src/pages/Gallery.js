94815dbb479f691e69d70c98494bec9f3a088fec
danielpedersen <danielweisspedersen95@hotmail.com>
2022-12-07 22:38:58 +0100

    replaced fetch from local file to fetch from url

--- src/pages/Gallery.js
+++ src/pages/Gallery.js
@@ -1,6 +1,17 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
-function Gallery () {
+function Gallery() {
+    const [data, setData] = useState([]); // store data collection
+
+    // fetch data
+    fetch("https://jsonplaceholder.typicode.com/photos")
+            .then((response) => response.json())
+            .then((json) => {
+            console.log(json);
+            setData(json);
+         }).then(error => {
+            console.log("something went wrong: " + error)
+    })
 
     useEffect(() => {
         document.title = 'Gallery';
@@ -8,9 +19,20 @@ function Gallery () {
 
     return (
         <div className="container">
-            <p>Insert gallery here....</p>
+            <div className="row">
+                <h1 className="text-center">Gallery</h1>
+            {data.map(item => {
+               return (
+                   <div className="col">
+                       <img src={item.thumbnailUrl} alt="" style={{marginTop: 30}}/>
+                   </div>
+               )
+            })}
+            </div>
+            <div>
+            </div>
         </div>
-    )
+    );
 }
 
 export default Gallery;
