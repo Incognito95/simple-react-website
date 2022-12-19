9aae3dac8a3a847027741ddae13d590fb8e69497
danielpedersen <danielweisspedersen95@hotmail.com>
2022-12-19 23:18:08 +0100

    added a hero section on the error page

--- src/pages/ErrorPage.js
+++ src/pages/ErrorPage.js
@@ -6,16 +6,18 @@ function ErrorPage() {
         document.title = '404';
     }, []);
 
+   
+    
     return (
-        <div class="container text-center">
-            <span class="number">4</span>
-            <div class="circle">
-                <div class="holder"></div>
-            </div>
-            <span class="number">4</span>
-            <p>Page not found!</p>
-            <a href="/" className="btn btn-success">Go back to the homepage!</a>
+    <div className="error-bg-color">
+        <div class="p-5 mb-4 error-bg">
+        <div class="container-fluid py-5">
+            <h1 class="display-5 fw-bold text-white mt-5">404</h1>
+            <p class="col-md-8 fs-4 text-white">Page not found!</p>
+            <a href="/" class="btn btn-success">Go to the homepage</a>
         </div>
+    </div>
+    </div>
     )
 }
 
